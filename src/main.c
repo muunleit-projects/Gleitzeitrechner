3d06afe4d00d005c95817ddc9e255e5a537d5e87
malte <muunleit@gmail.com>
2022-05-08 22:52:58 +0200

    correction on the comments

--- src/main.c
+++ src/main.c
@@ -6,33 +6,38 @@
 #include "jaNeinAbfrage.h"
 
 int main() {
-  unsigned int arbeitsbeginn_stunden = 0;  // Eingestempelt, Stundenanteil
-  unsigned int arbeitsbeginn_minuten = 0;  // Eingestempelt, Minutenanteil
-  unsigned int arbeitszeit_stunden = 0;    // Arbeitszeit, Stundenanteil
-  unsigned int arbeitszeit_minuten = 0;    // Arbeitszeit, Minutenanteil
+  unsigned int arbeitsbeginn_stunden = 0;
+  unsigned int arbeitsbeginn_minuten = 0;
+  unsigned int arbeitszeit_stunden = 0;
+  unsigned int arbeitszeit_minuten = 0;
   unsigned int pause_stunden = 0;
   unsigned int pause_minuten = 0;
   char pause_gesetzlich = 0;
-  time_t jetzt = 0;                // aktuelle Zeit (Sekunden seit 01.01.1970)
-  struct tm* arbeitsende = NULL;   // Arbeitsende (Konstrukt mit Datum/Uhrzeit)
-  float zeit_bis_arbeitsende = 0;  // von "jetzt" bis zum Arbeitsende in Stunden
+  time_t jetzt = 0;
+  struct tm* arbeitsende = NULL;
+  float zeit_bis_arbeitsende = 0;
 
-  // Benutzerreingabe
+  /* Asking the user to input the starting time. */
   printf("Geben Sie die Anfangszeit ein ");
   while (eingabeZeit(&arbeitsbeginn_stunden, &arbeitsbeginn_minuten) != 0) {
     printf("Versuchen Sie es noch einmal ");
   }
 
+  /* Asking the user to input the working time. */
   printf("Geben Sie die Laenge der Arbeitszeit ein ");
   while (eingabeZeit(&arbeitszeit_stunden, &arbeitszeit_minuten) != 0) {
     printf("Versuchen Sie es noch einmal ");
   }
 
+  /* Asking the user if the length of the pause is according to the legal
+   * requirements. */
   printf("Entspricht die Laenge ihrer Pause den gesetzlichen Vorgaben? ");
   while (jaNeinAbfrage(&pause_gesetzlich) != 0) {
     printf("Versuchen Sie es noch einmal ");
   }
 
+  /* Setting the Asking the length of the pause to the legal requirements or
+   * asking the user to input the pause time. */
   if (pause_gesetzlich == 'j') {
     if (arbeitszeit_stunden >= 9) {
       pause_minuten = 45;
@@ -50,28 +55,31 @@ int main() {
     }
   }
 
-  // aktuelle Zeit ermitteln und in "jetzt" speichern
+  /* Getting the current time and storing it in the variable `jetzt`. */
   time(&jetzt);
 
-  // Zeit-Konstrukt aus der Zeit in "jetzt" erstellen
+  /* Converting the time_t value `jetzt` to a struct tm value in local format.
+   */
   arbeitsende = localtime(&jetzt);
 
-  // Tageswechsel überprüfen und Tag des Arbeitsbeginns anpassen
+  /* It checks if the current hour is smaller than the starting hour. If it is,
+   * it subtracts one from the day. */
   if (arbeitsende->tm_hour < (int)arbeitsbeginn_stunden) {
     arbeitsende->tm_mday -= 1;
   }
 
-  // Arbeitsende aus Arbeitsbeginn, Arbeitszeit und Pause ermitteln
+  /* Adding the starting time, the working time and the pause time to the
+   * current time. */
   arbeitsende->tm_hour =
       arbeitsbeginn_stunden + arbeitszeit_stunden + pause_stunden;
   arbeitsende->tm_min =
       arbeitsbeginn_minuten + arbeitszeit_minuten + pause_minuten;
 
-  // Differenz zwischen Jetzt und Arbeitsende ermitteln
-  // Arbeitsende-Kontrukt aufräumen!!! Doppelte Nutzen aus mktime()
+  /* It calculates the time difference between the current time and the time
+   * when the work ends and clean Up Arbeitsende-Kontrukt in one functioncall */
   zeit_bis_arbeitsende = difftime(mktime(arbeitsende), jetzt) / 3600;
 
-  // Ausgabe
+  /* Printing the time when the work ends and the time left until the work ends. */
   printf(
       "\nBei einer Arbeitszeit von %d:%02dh und %d:%02dh Pause "
       "waere gegen %02d:%02d Arbeitsende\n",
@@ -81,7 +89,7 @@ int main() {
   printf("\nNoch %d Stunden und %.0f Minuten ", (int)zeit_bis_arbeitsende,
          (zeit_bis_arbeitsende - (int)zeit_bis_arbeitsende) * 60);
 
-  // Fenster offen halten bis der Benutzer ENTER drückt
+  /* Waiting for the user to press a key. */
   getchar();
 
   return EXIT_SUCCESS;
